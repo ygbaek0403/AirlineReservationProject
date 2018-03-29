cdf66355d0416c8e71bfcd40376c509f5e5dc827
ygbaek <ygbaek@192.168.1.123>
2018-03-29 14:59:43 -0400

    main class update

--- AirlineReservationProject/src/application/Main.java
+++ AirlineReservationProject/src/application/Main.java
@@ -76,6 +76,18 @@ public class Main extends Application {
 
     }
 	 
+	@Override
+	public void start2(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
